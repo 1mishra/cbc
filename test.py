2d01dd9d241f35ace892a0164ca03208df165dc8
Reginald Long <reggylong@gmail.com>
2016-10-16 17:58:55 -0700

    making sam compressor

--- test.py
+++ test.py
@@ -22,10 +22,10 @@ def process_sam(filename):
   for line in f:
     if line[0] == '@':
       continue
+    fields = line.split()
     if int(fields[1]) & 4 == 4:
         # unmapped read
         continue
-    fields = line.split()
     if 16 & int(fields[1]) > 0:
       reads.append(reverse_complement(fields[9].strip()))
     else:
